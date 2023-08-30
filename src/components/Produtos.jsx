514cdea8cce9264d9827045b7858bab0c05ea539
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-08-29 22:51:41 -0300

    Add jsx

---
+++ src/components/Produtos.jsx
@@ -0,0 +1,15 @@
+export default function Produtos(){
+
+ 
+
+    return(
+
+        <main>
+
+            <h1>Produtos</h1>
+
+        </main>
+
+    )
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
-
 import ReactDOM from 'react-dom/client'
-
 import App from './App.jsx'
-
 import './index.css'
+import { createBrowserRouter, RouberProvider } from 'react-router-dom'
+import Home from './components/Home.jsx';
+import Produtos from './components/Produtos.jsx';
+import EditarProduto from './components/EditarProduto.jsx';
+import Error from './components/Error.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ const router = createBrowserRouter([
+
+  {path:"/" , element:<App/>}
+
+])
+
+ ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
+
     <App />
+
   </React.StrictMode>,
+
 )
 
+ 
