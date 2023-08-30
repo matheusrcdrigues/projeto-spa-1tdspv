9364aac8a75a174316d57c0b425c402e1ad9cf90
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-08-29 22:43:33 -0300

    Atualização geral

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import reactLogo from "./assets/react.svg";
+import Cabecalho from "./components/Cabecalho";
+import Conteudo from "./components/Conteudo";
+import Rodape from "./components/Rodape";
 
 export default function App() {
-// AREA DECLARATIVA
+  
+  //ÁREA DECLARATIVA
 
+  let reactLogoTextoAlt = "Logo do React";
 
   return (
-  <>
-  {/*AREA IMPERATIVA */}
+    <>
+      {/* Área imperativa! */}
+      <div>
+        {/* Um cabeçalho com um header, um h1, uma lista ul com 3 itens com 3 links. */}
+      <Cabecalho/>
+        {/* Uma seção com uma div com 3 parágrafos, com 3 linhas de lorem, uma imagem */}
 
-  <div>
-    <h1>Vite + React</h1>
-    <h2>Conseguimos!</h2>
-    <p>Olá Mundo!</p>
-  </div>
-  </>
+      <Conteudo reactLogoProps={reactLogo} reactLogoTextoAltProps={reactLogoTextoAlt} />
+        
+        {/* Um rodapé com uma div, uma lista ul com 3 itens e links para redes sociais */}
+        <Rodape/>
+
+      </div>
+    </>
   )
 }
