4214036629282e6892893bfa60ca9b00e178745e
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-08-29 22:51:29 -0300

    Add jsx em componentes

---
+++ src/components/EditarProduto.jsx
@@ -0,0 +1,12 @@
+export default function EditarProduto(){
+    return(
+
+        <main>
+
+            <h1>Editar o Produto</h1>
+
+        </main>
+
+    )
+
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+export default function Home(){
+
+ 
+
+    return(
+
+        <main>
+
+            <h1>Home</h1>
+
+        </main>
+
+    )
+
+}
